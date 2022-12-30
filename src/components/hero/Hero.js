4b10027bf3bd5ab751e5ab84c4d32c628cde4e44
Maruf Khan <maruf1611khan@gmail.com>
2022-12-30 19:23:03 +0530

    hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,6 +4,35 @@ import './Hero.css'
 function Hero() {
   return (
     <div className='hero'>
+
+      <div className="wrapper">
+        <div className="left">
+          <div className="gandharva">
+            Gandharva
+          </div>
+          <div className="party">
+              Let's Start The Party
+          </div>
+          <div className="slogan">
+            Slogan Goes here!
+          </div>
+          <div className="live">
+            LIVE IN PUNE | FROM JAN 26-29, 2023
+          </div>
+          <div className="register">
+            <button className='regbtn'>
+              Register
+            </button>
+          </div>
+        </div>
+
+        <div className="right">
+          Spinning thing lmao
+        </div>
+      </div>
+
+      
+
     </div>
   )
 }
